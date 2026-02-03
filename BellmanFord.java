aa0a5ae06ccca29cfaecb70f39091dff13252594
Diyana <studybuddy206@gmail.com>
2026-02-03 17:40:23 +0530

    Rename Lab4BellmanFord class to BellmanFord

--- BellmanFord.java
+++ BellmanFord.java
@@ -2,7 +2,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Lab4BellmanFord {
+public class BellmanFord {
         public static void main(String[] args) {
                 Scanner sc = new Scanner(System.in);
                 int n;
